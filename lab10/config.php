12360ee19390e140c10ad6caacf8bb92fc1a0459
LiuPeiQi20001231 <liupeiqi20001231@163.com>
2020-06-08 01:28:07 +0800

    upload lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'liuge');
+define('DBPASS', 'liupeiqi1231');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
